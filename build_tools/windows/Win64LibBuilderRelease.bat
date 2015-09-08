d940c79de726641a7da1b2c8b7b12fa7cc7dd09f
Andrius Tarasovas <andrius.tarasovas@visma.com>
2015-09-08 16:01:41 +0300

    Lib builder for windows, lib upgrades.

--- build_tools/windows/Win64LibBuilderRelease.bat
+++ build_tools/windows/Win64LibBuilderRelease.bat
@@ -1,10 +1,11 @@
+cd ../../
 rmdir /s /q "build"
 rmdir /s /q "libs/win64release"
 mkdir "build"
 mkdir "libs/win64release"
 cd "build"
-call vcvars64.bat
-cmake ../ -DCMAKE_BUILD_TYPE=RELEASE -G "NMake Makefiles"
+call ../build_tools/windows/vcvars64.bat
+cmake ../libs/ -DCMAKE_BUILD_TYPE=RELEASE -G "NMake Makefiles"
 nmake
 cd "../libs/boost/"
 start /wait b2 -j8 --without-python --build-dir="../../build"  --toolset=msvc architecture=x86 address-model=64  link=static threading=multi release
