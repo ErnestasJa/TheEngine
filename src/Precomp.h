bdc6703551addf6bfc71e7ac362f9281572733da
Andrius Tarasovas <murloc992@gmail.com>
2015-10-18 00:12:45 +0300

    Unneeded stuff removed.

--- src/Precomp.h
+++ src/Precomp.h
@@ -21,22 +21,7 @@
 
 // OPENGL RELATED LIBS
 #include <GLXW/glxw.h>
-
-#if defined(_WIN32)
-#define GLFW_EXPOSE_NATIVE_WIN32
-#define GLFW_EXPOSE_NATIVE_WGL
-#define OVR_OS_WIN32
-#elif defined(__APPLE__)
-#define GLFW_EXPOSE_NATIVE_COCOA
-#define GLFW_EXPOSE_NATIVE_NSGL
-#define OVR_OS_MAC
-#elif defined(__linux__)
-#define GLFW_EXPOSE_NATIVE_X11
-#define GLFW_EXPOSE_NATIVE_GLX
-#define OVR_OS_LINUX
-#endif
 #include <GLFW/glfw3.h>
-#include <GLFW/glfw3native.h>
 
 #include "glm.hpp"
 #include "gtx/norm.hpp"
